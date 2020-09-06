1cc7054fd4b7d6faef9bb635179faa19635f986b
samguml <70164625+samguml@users.noreply.github.com>
2020-09-06 14:52:15 -0400

    Update module_extern.h

--- module_extern.h
+++ module_extern.h
@@ -1,1 +1,9 @@
+/**
+@file module_extern.h
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief Would include extern defintions for dynchronizers and extern defintions for variables, but there are none. 
+@Date 9/6/2020
 
+@copyright Copyright (c) 2020
+
+**/
