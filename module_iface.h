f2b697aef719caea10698d721109cb3de012bcb7
samguml <70164625+samguml@users.noreply.github.com>
2020-09-06 14:54:18 -0400

    Update module_iface.h

--- module_iface.h
+++ module_iface.h
@@ -1,7 +1,7 @@
 /**
 @file module_iface.h
 @Author: Samuel Gregory (samuel_gregory@student.uml.edu)
-@brief 
+@brief Would include extern prototypes for module_int, module_exit, module_exec, and pthread-cond-t but there are none
 @Date 9/3/2020
 
 @copyright Copyright (c) 2020
