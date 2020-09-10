c335435c70a6c3fdb9346a9b76e5b414f45d7d43
samguml <70164625+samguml@users.noreply.github.com>
2020-09-10 15:03:05 -0400

    Delete module_iface.h

--- module_iface.h
+++
@@ -1,9 +0,0 @@
-/**
-@file module_iface.h
-@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
-@brief Would include extern prototypes for module_int, module_exit, module_exec, and pthread-cond-t but there are none
-@Date 9/3/2020
-
-@copyright Copyright (c) 2020
-
-**/
