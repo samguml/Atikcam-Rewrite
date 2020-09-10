63fac794f9cd62c82a18b953cea2b47eb8c9a610
samguml <70164625+samguml@users.noreply.github.com>
2020-09-10 16:57:46 -0400

    Update housekeeping.h

--- modules/housekeeping.h
+++ modules/housekeeping.h
@@ -1,1 +1,11 @@
+/**
+@file housekeeping.h
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief main module header, contains all function prototypes of housekeeping module
+@Date 9/10/2020
+@copyright Copyright (c) 2020
+**/
+
+//Function prototypes are as follows
+//Note: no prototypes for functions were found, ask aabout this
 
