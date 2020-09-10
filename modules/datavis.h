144bf119e9c361f48533f09e1ae78b047bd202fd
samguml <70164625+samguml@users.noreply.github.com>
2020-09-10 17:05:40 -0400

    Update datavis.h

--- modules/datavis.h
+++ modules/datavis.h
@@ -1,1 +1,10 @@
+  /**
+@file camera.h
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief main module header, contains all function prototypes of datavis module
+@Date 9/10/2020
+@copyright Copyright (c) 2020
+**/
 
+//Function prototypes are as follows
+//Found none
