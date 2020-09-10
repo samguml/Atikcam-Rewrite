adaf1fe64dc0df35757bc28033c5beba0e660bc4
samguml <70164625+samguml@users.noreply.github.com>
2020-09-10 16:53:56 -0400

    Update camera.h

--- modules/camera.h
+++ modules/camera.h
@@ -1,1 +1,10 @@
+/**
+@file camera.h
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief main module header, contains all function prototypes of camera module
+@Date 9/10/2020
+@copyright Copyright (c) 2020
+**/
 
+//Function prototypes are as follows
+//Note:no function prototpyes were found in the body of the module. Ask about this.
