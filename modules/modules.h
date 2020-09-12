a74896eaafca7d0e816332e3becb119f7770bfb0
samguml <70164625+samguml@users.noreply.github.com>
2020-09-12 19:11:14 -0400

    Update modules.h

--- modules/modules.h
+++ modules/modules.h
@@ -1,1 +1,51 @@
+/**
+@file modules.h
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief 
+@Date 9/12/2020
+@copyright Copyright (c) 2020
+**/
 
+//Note this code may be imperfect
+
+//Register modules with init function
+
+init_func module_init[] = {};
+
+//Number of modules with init functions
+
+const int num_init = sizeof(module_init) / sizeof(init_func);
+//Register modules with exit function
+
+exit_func module_exit[] = {};
+//Number of modules with exit functions
+const int num_exit = sizeof(module_exit) / sizeof(exit_func);
+
+//Register modules with exec functions 
+
+void *module_exec[] = {
+    
+#ifdef DATAVIS
+    ,
+    datavis_thread
+#endif
+#ifdef SITL
+    ,
+    sitl_comm
+#endif
+};
+//Number of modules with exec functions
+
+const int num_systems = sizeof(module_exec) / sizeof(void *);
+
+//Allows for modules to be woken up
+
+pthread_cond_t *wakeups[] = {
+#ifdef SITL
+    &data_available
+#endif 
+#ifdef DATAVIS
+    ,
+    &datavis_drdy
+#endif 
+};
