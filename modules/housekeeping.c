2eba7e909ae6fd9677e2ec6a4067e725068ae9f5
samguml <70164625+samguml@users.noreply.github.com>
2020-09-13 13:54:47 -0400

    Update housekeeping.c

--- modules/housekeeping.c
+++ modules/housekeeping.c
@@ -6,5 +6,40 @@
 @copyright Copyright (c) 2020
 **/
 
-//function defintions
-//need to ask about void * functions
+//Function prototypes are as follows
+
+inline void put_data ( ostream & str , unsigned short val )
+{
+	shb x ;
+	x.s = val ;
+	for ( char i = 0 ; i < sizeof(x.b) ; i++ )
+		str << x.b[i] ;
+}
+inline void put_data ( ostream & str , unsigned long long int val )
+{
+	llb x ;
+	x.l = val ;
+	for ( char i = 0 ; i < sizeof(x.b) ; i++ )
+		str << x.b[i] ;
+}
+inline void put_data ( ostream & str , float val )
+{
+	flb x ;
+	x.f = val ;
+	for ( char i = 0 ; i < sizeof(x.b) ; i++ )
+		str << x.b[i] ;
+}
+inline void put_data ( ostream & str , char val )
+ {
+	 str << val ;
+ }
+
+unsigned long long int timenow()
+  {
+	return ((std::chrono::duration_cast<std::chrono::milliseconds> 
+	((std::chrono::time_point_cast<std::chrono::milliseconds>
+	(std::chrono::system_clock::now())).time_since_epoch())).count()) ;
+}
+
+//again need to ask about this one
+void * housekeeping_thread(void *t)
