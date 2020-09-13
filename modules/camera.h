5aec8ff6172328f006ef312a88f41ecb439586e1
samguml <70164625+samguml@users.noreply.github.com>
2020-09-13 13:57:48 -0400

    Update camera.h

--- modules/camera.h
+++ modules/camera.h
@@ -7,4 +7,25 @@
 **/
 
 //Function prototypes are as follows
-//Note:no function prototpyes were found in the body of the module. Ask about this.
+
+void convert_to_packet(image * a , datavis_p * b) 
+inline void put_data ( ostream & str , unsigned short val )
+inline void put_data ( ostream & str , unsigned long long int val )
+inline void put_data ( ostream & str , float val )
+inline void put_data ( ostream & str , char val )
+  
+int save(const char *fileName , image * data)
+  
+void sys_reboot(void)
+  
+char space_left(void)
+  
+unsigned long long int timenow()
+  
+double find_optimum_exposure ( unsigned short * picdata , unsigned int imgsize , double exposure )
+  
+bool snap_picture ( AtikCamera * device , unsigned pixX , unsigned pixY , unsigned short * data , double exposure  )
+  
+void * camera_thread(void *t)
+  
+  
