afea212d76f2d7739a1d0842d90bbacfbdfeddad
samguml <70164625+samguml@users.noreply.github.com>
2020-09-13 13:47:54 -0400

    Update datavis.c

--- modules/datavis.c
+++ modules/datavis.c
@@ -1,1 +1,10 @@
+  /**
+@file datavis.c
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief 
+@Date 9/13/2020
+@copyright Copyright (c) 2020
+**/
 
+//function definitions
+//need to ask about void * functions
