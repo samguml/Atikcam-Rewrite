269543345d5a86828df3ce81969bae3ca9a1a643
samguml <70164625+samguml@users.noreply.github.com>
2020-09-13 13:45:28 -0400

    Update housekeeping.c

--- modules/housekeeping.c
+++ modules/housekeeping.c
@@ -1,1 +1,10 @@
+/**
+@file housekeeping.c
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief 
+@Date 9/13/2020
+@copyright Copyright (c) 2020
+**/
 
+//function defintions
+//need to ask about void * functions
