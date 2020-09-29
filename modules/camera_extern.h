18750555ee9494bf6fac2daa7ca6ee1eb0c0454a
samguml <70164625+samguml@users.noreply.github.com>
2020-09-28 20:18:20 -0400

    Update camera_extern.h

--- modules/camera_extern.h
+++ modules/camera_extern.h
@@ -1,1 +1,13 @@
+/*
+@file camera_extern.h
+@author: 9/28/20
+@brief: contains extern defnitons for shared variables and synchronizers between modules
+*/ 
 
+#include <camera.h>
+#include <pthread.h>
+
+
+extern minshortexposure;
+extern maxshortexposure;
+extern pix_bin;
