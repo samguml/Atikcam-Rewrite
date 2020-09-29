f5fdf020989f853cd34c4338373a338d4e25ff83
samguml <70164625+samguml@users.noreply.github.com>
2020-09-28 20:49:45 -0400

    Update main.c

--- modules/main.c
+++ modules/main.c
@@ -1,1 +1,14 @@
+/**
+@file main.c
+@Author: Samuel Gregory (samuel_gregory@student.uml.edu)
+@brief provides threat init, spawn, join, destroy. Signal handler for all threads. Would contain error printer but that feature will be phased out 
+@Date 9/28/2020
 
+**/
+#include <main.h>
+#include <module.c>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <pthread.h>
+#include <signal.h>
