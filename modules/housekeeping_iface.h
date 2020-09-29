37522b70a2dc04f82c33f2a52026b17d16813a1a
samguml <70164625+samguml@users.noreply.github.com>
2020-09-28 21:14:17 -0400

    Update housekeeping_iface.h

--- modules/housekeeping_iface.h
+++ modules/housekeeping_iface.h
@@ -1,1 +1,16 @@
+/*
+ @file housekeeping_iface.h
+ @author Samuel Gregory
+ @brief housekeeping thread externs for main
+ @date 9/28/20
+ 
+ 
+ */
+#ifndef HOUSEKEEPING_IFACE_H
+#define HOUSEKEEPING_IFACE_H
+#include <housekeeping_extern.h>
+void *housekeeping_thread(void *);
+#endif 
+
+
 
