9bbd082d03a262609479be69db34cdf7d2ab5617
samguml <70164625+samguml@users.noreply.github.com>
2020-09-28 20:53:29 -0400

    Update main.h

--- modules/main.h
+++ modules/main.h
@@ -1,1 +1,8 @@
+/**
+@file main.h
+@Author: Samuel Gregory 
+@brief has source code for every module. Also holds extern definitions. Would contian error printer prototype, but that feature will not be included.
+@Date 9/28/2020
+
+**/
 
