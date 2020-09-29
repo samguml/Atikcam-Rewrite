763f7919d9c358f4312500b8e59e2384c09e51d4
samguml <70164625+samguml@users.noreply.github.com>
2020-09-28 21:12:48 -0400

    Update camera_iface.h

--- modules/camera_iface.h
+++ modules/camera_iface.h
@@ -1,1 +1,15 @@
+/*
+ @file camera_iface.h
+ @author Samuel Gregory
+ @brief camera thread externs for main
+ @date 9/28/20
+ 
+ 
+ */
+#ifndef CAMERA_IFACE_H
+#define CAMERA_IFACE_H
+#include <camera_extern.h>
+void *camera_thread(void *);
+#endif 
+
 
