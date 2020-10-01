982e68b65d327d1f1932448d41295620247b590a
samguml <70164625+samguml@users.noreply.github.com>
2020-09-30 21:16:33 -0400

    Update modules.h

--- modules/modules.h
+++ modules/modules.h
@@ -6,7 +6,7 @@
 @copyright Copyright (c) 2020
 **/
 
-# include <stdio.h>
+#include <stdio.h>
 #include <housekeeping_iface.h>
 #include <camera_iface.h>
 #include <pthread.h>
@@ -20,37 +20,15 @@ init_func module_init[] = {};
 //Number of modules with init functions
 
 const int num_init = sizeof(module_init) / sizeof(init_func);
-//Register modules with exit function
+//
+destroy_func module_destroy[] = {};
+//gives # of modules with a destroy function 
 
-exit_func module_exit[] = {};
-//Number of modules with exit functions
-const int num_exit = sizeof(module_exit) / sizeof(exit_func);
+const int num_destroy = sizeof(module_destroy) / sizeof(destroy_func);
+//registers exec functions of modules
+void *module_exec[] = {};
 
-//Register modules with exec functions 
-//slightly unsure of the DATAVIS parts
-void *module_exec[] = {
-    
-#ifdef DATAVIS
-    ,
-    datavis_thread
-#endif
-#ifdef SITL
-    ,
-    sitl_comm
-#endif
-};
-//Number of modules with exec functions
-
+//gives # of enabled modules
 const int num_systems = sizeof(module_exec) / sizeof(void *);
 
-//Allows for modules to be woken up
-
-pthread_cond_t *wakeups[] = {
-#ifdef SITL
-    &data_available
-#endif 
-#ifdef DATAVIS
-    ,
-    &datavis_drdy
-#endif 
-};
+//
