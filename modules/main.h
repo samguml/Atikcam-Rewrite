c22255293084bc4766ea51f2bdb95c02d752eeb7
samguml <70164625+samguml@users.noreply.github.com>
2020-09-30 21:59:30 -0400

    Update main.h

--- modules/main.h
+++ modules/main.h
@@ -1,15 +1,26 @@
 /**
 @file main.h
 @Author: Samuel Gregory 
-@brief has source code for every module. Also holds extern definitions. Would contian error printer prototype, but that feature will not be included.
+@brief has source code for every module. Also holds extern definitions. Would contain error printer prototype, but that feature will not be included.
 @Date 9/28/2020
-
 **/
+//Note all modules include main.h in their .c file!
 
 #ifndef MAIN_H
 #define MAin_H
 #endif
 
-/*extern*/ overheat(int signum)
+//extern definition of done (controls loops for all threads)
+extern volatile sig_atomic_t done = 0 ;
+
+//extern definition of thread local errno/custom 
+extern ofstream templog ;
+extern ofstream camlog ;
+extern ofstream errlog 
+
+//extern defintion of local signal handler? 
+extern overheat(int signum)
+
+
 //Signal Handler prototype- i think
 void term (int signum);
