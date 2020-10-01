548ef706f1ebce7e7addfca0414afaec2e9d5c63
samguml <70164625+samguml@users.noreply.github.com>
2020-09-30 20:27:43 -0400

    Update datavis.h

--- modules/datavis.h
+++ modules/datavis.h
@@ -9,6 +9,20 @@
 #ifndef DATAVIS_H
 #define DATAVIS_H
 #endif
+
+typedef struct {
+	uint64_t tnow ; //timestamp in ms
+	float exposure ; //exposure in ms
+	unsigned short pixx ; //348
+	unsigned short pixy ; //260
+	unsigned char pixbin ;
+	short ccdtemp ; // temp in C * 100
+	short boardtemp ;
+	short chassistemp ;
+	unsigned char picdata[90480];
+} datavis_p ;
+
+
 //Function prototypes are as follows
 #ifndef PACK_SIZE
 #define PACK_SIZE sizeof(datavis_p)
