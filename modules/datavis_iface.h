04d7afc5d60a02867aedce559f6d00b28dd88772
samguml <70164625+samguml@users.noreply.github.com>
2020-10-01 22:00:50 -0400

    Update datavis_iface.h

--- modules/datavis_iface.h
+++ modules/datavis_iface.h
@@ -1,1 +1,14 @@
+/*
+ @file datavis_iface.h
+ @author Samuel Gregory
+ @brief datavis thread externs for main
+ @date 10/1/20
+ 
+ 
+ */
+#ifndef DATAVIS_IFACE_H
+#define DATAVIS_IFACE_H
+
+void *datavis_thread(void *);
+#endif 
 
