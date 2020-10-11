ee19b463f6b8683cda60063828cab6d144339082
samguml <70164625+samguml@users.noreply.github.com>
2020-10-11 13:27:38 -0400

    Update datavis_extern.h

--- include/datavis_extern.h
+++ include/datavis_extern.h
@@ -1,1 +1,14 @@
-
+/*
+ @file datavis_extern.h
+ @author Samuel Gregory
+ @brief 
+ @date 10/11/20
+ 
+ */
+#ifndef DATAVIS_EXTERN_H
+#define DATAVIS_EXTERN_h
+#include <datavis.h>
+#include <pthread.h>
+#ifndef DATAVIS_IFACE_H
+#endif
+#endif
