e4ad254e7d77c57b6d8ef97c22e70c77fc61ca40
samguml <70164625+samguml@users.noreply.github.com>
2020-10-11 16:06:25 -0400

    Update camera.h

--- include/camera.h
+++ include/camera.h
@@ -27,24 +27,28 @@ typedef struct image {
 	unsigned char unused2[1792] ; //padding to round off to 708*4096 bytes
 } image ; //size 708*4096
 
-void convert_to_packet(image * a , datavis_p * b)
+void convert_to_packet(image * a , datavis_p * b);
 
-inline void put_data ( ostream & str , unsigned short val )
-inline void put_data ( ostream & str , unsigned long long int val )
-inline void put_data ( ostream & str , float val )
-inline void put_data ( ostream & str , char val ) 
+inline void put_data ( ostream & str , unsigned short val );
+inline void put_data ( ostream & str , unsigned long long int val );
+inline void put_data ( ostream & str , float val );
+inline void put_data ( ostream & str , char val ) ;
   
-int save(const char *fileName, image* data)
+int save(const char *fileName, image* data);
  
-void sys_reboot(void)
+void sys_reboot(void);
+
+char space_left(void);
+
+unsigned long long int timenow();
  
-double find_optimum_exposure ( unsigned short * picdata , unsigned int imgsize , double exposure )
+double find_optimum_exposure ( unsigned short * picdata , unsigned int imgsize , double exposure );
   
-bool snap_picture ( AtikCamera * device , unsigned pixX , unsigned pixY , unsigned short * data , double exposure  )
+bool snap_picture ( AtikCamera * device , unsigned pixX , unsigned pixY , unsigned short * data , double exposure  );
   
-char space_left(void)
+char space_left(void);
 
  
-void * camera_thread(void *t)
+void * camera_thread(void *t);
   
   
