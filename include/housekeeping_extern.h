8ab519aaf6e305c248c129d40ec015d79aceddf0
samguml <70164625+samguml@users.noreply.github.com>
2020-10-12 00:54:20 -0400

    Update housekeeping_extern.h

--- include/housekeeping_extern.h
+++ include/housekeeping_extern.h
@@ -1,15 +1,23 @@
 /*
 @file camera_extern.h
-@author: 9/28/20
+@author: Samuel Gregory
+@Date: 9/28/20
 @brief: contains extern definitons for shared variables and synchronizers between modules
 */ 
 
-#include <housekeeping.h>
+#include <housekeeping.h> //This may not be necessary, but is included just in case. May be subject to change 
 #include <pthread.h>
+#ifndef HOUSEKEEPING_EXTERN_H
+#define HOUSEKEEPING_EXTERN_H
+#endif
 
 extern volatile int boardtemp;
+
 extern volatile int chassistemp;
+
 extern bool cam_off;
+
 extern volatile unsigned long long camofftime:
+
 extern volatile bool ccdoverheat;
 
