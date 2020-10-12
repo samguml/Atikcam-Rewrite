541b32c98d8725710b2eea4adda67b41ebfda960
samguml <70164625+samguml@users.noreply.github.com>
2020-10-12 01:00:18 -0400

    Update camera_iface.h

--- include/camera_iface.h
+++ include/camera_iface.h
@@ -1,7 +1,7 @@
 /*
  @file camera_iface.h
  @author Samuel Gregory
- @brief: This file contains the extern prototypes for module_init, module_exec, module_exit, and pthread_cond_t
+ @brief: This file contains the extern prototypes for module_init, module_exec, module_exit, and pthread_cond_t for the camera thread
  @date 9/28/20
  
  
