b61f528137ded4539a979ed6395d9090cf5fc60d
samguml <70164625+samguml@users.noreply.github.com>
2020-10-12 00:54:53 -0400

    Update camera_extern.h

--- include/camera_extern.h
+++ include/camera_extern.h
@@ -5,7 +5,7 @@
 @brief: contains extern defnitons for shared variables and synchronizers between modules
 */ 
 
-#include <camera.h>
+#include <camera.h>  //This may not be necessary, but is included just in case. May be subject to change 
 #include <pthread.h>
 #ifndef CAMERA_EXTERN_H
 #define CAMERA_EXTERN_H
