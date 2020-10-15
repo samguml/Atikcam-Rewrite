58ec7b52eada73daf8773ec9386e15aafbff4896
samguml <70164625+samguml@users.noreply.github.com>
2020-10-15 19:38:56 -0400

    Update macros.h

--- include/macros.h
+++ include/macros.h
@@ -1,11 +1,20 @@
 /*
 @file macros.h
-@author: 9/28/20
-@brief: contains all macros
+@author: Samuel Gregory
+@datee: 10/15/20
+@brief: contains the funtions needed throughout the flightcam software 
 */ 
+
+//Note:this software is still subject to change-macros.h is one of the files not mentioned in the desgin pdf
 
 #include <stdio.h>
 
+#ifndef _MACROS_H
+#define _MACROS_H
+#endif
+
+
+//Global Macros
 #define MAX 1
 
 #ifndef MAX_ALLOWED_EXPOSURE
@@ -17,7 +26,7 @@
 #endif
 
 #ifndef PIC_TIME_GAP
-#define PIC_TIME_GAP 10.0 
+#define PIC_TIME_GAP 10.0 // minimum gap between images in seconds
 #endif
 
 #ifndef PIX_MEDIAN
@@ -33,7 +42,7 @@
 #endif
 
 #ifndef CCD_COOLDOWN_TIME
-#define CCD_COOLDOWN_TIME 60*1000 
+#define CCD_COOLDOWN_TIME 60*1000 // in milliseconds
 #endif
 
 #ifndef PORT
@@ -43,47 +52,80 @@
 #ifndef SERVER_IP
 #define SERVER_IP "192.168.1.2"
 #endif //SERVER_IP
+//End of global macros
 
 #ifndef PACK_SIZE
 #define PACK_SIZE sizeof(datavis_p)
 #endif
 
-#ifndef PERCENTILE
-#define PERCENTILE 90.0
+//Saving FITS image
+#ifndef NOSAVEFITS
+int save(const char *fileName , image * data) {
+  fitsfile *fptr;
+  int status = 0, bitpix = USHORT_IMG, naxis = 2;
+  int bzero = 32768, bscale = 1;
+  long naxes[2] = { (long)data->pixx, (long)data->pixy };
+  if (!fits_create_file(&fptr, fileName, &status)) {
+	fits_set_compression_type(fptr, RICE_1, &status) ;
+    fits_create_img(fptr, bitpix, naxis, naxes, &status);
+    fits_write_key(fptr, TSTRING, "PROGRAM", (void *)"sk_flight", NULL, &status);
+    fits_write_key(fptr, TUSHORT, "BZERO", &bzero, NULL, &status);
+    fits_write_key(fptr, TUSHORT, "BSCALE", &bscale, NULL, &status);
+    fits_write_key(fptr, TFLOAT, "EXPOSURE", &(data->exposure), NULL, &status);
+    fits_write_key(fptr, TSHORT, "TEMP", &(data->ccdtemp), NULL, &status);
+    fits_write_key(fptr, TLONGLONG, "TIMESTAMP", &(data->tnow),NULL, &status);
+    long fpixel[] = { 1, 1 };
+    fits_write_pix(fptr, TUSHORT, fpixel, data->imgsize, data->picdata, &status);
+    fits_close_file(fptr, &status);
+	#ifdef SK_DEBUG
+    cerr << endl << "Camera Thread: Save: saved to " << fileName << endl << endl;
+	#endif
+  }
+  else {
+	  cerr << "Error: " << __FUNCTION__ << " : Could not save image." << endl ;
+	  errlog << "Error: " << __FUNCTION__ << " : Could not save image." << endl ;
+  }
+  return status ;
+}
+#else
+int save(const char *fileName, image* data)
+{
+	return false;
+}
 #endif
 
-#ifndef PIX_MEDIAN
-#define PIX_MEDIAN 40000.0
-#endif
-  
-#ifndef PIX_GIVE
-#define PIX_GIVE 5000.0
-#endif
 
-#ifndef TEMPLOG_LOCATION
-#define TEMPLOG_LOCATION "/home/pi/temp_log.bin"
-#endif
-
-#ifndef CAMLOG_LOCATION
-#define CAMLOG_LOCATION "/home/pi/cam_log.bin"
-#endif
-
-#ifndef CHASSISLOG_LOCATION
-#define CHASSISLOG_LOCATION "/home/pi/chassis_log.bin"
+//Fuction for shutting down and rebooting 
+#ifdef ENABLE_PWOFF
+void sys_poweroff(void)
+{
+	cerr << "Info: Poweroff instruction received!" << endl ;
+	sync() ;
+	setuid(0) ;
+	reboot(LINUX_REBOOT_CMD_POWER_OFF) ;
+}
+#else
+void sys_poweroff(void)
+{	
+	cerr << "Info: Poweroff instruction received!" << endl ;
+	exit(0);
+}
 #endif
 
-
-#ifndef BOARDLOG_LOCATION
-#define BOARDLOG_LOCATION "/home/pi/board_log.bin"
-#endif
-
-
-#ifndef BOARDLOG_LOCATION
-#define BOARDLOG_LOCATION "/home/pi/board_log.bin"
-#endif
-  
-  
- 
-  
+//Function for rebooting
+#ifdef ENABLE_REBOOT
+void sys_reboot(void)
+{
+	cerr << "Info: Reboot instruction received!" << endl ;
+	sync() ;
+	setuid(0) ;
+	reboot(RB_AUTOBOOT) ;
+}
+#else
+void sys_reboot(void)
+{
+	cerr << "Info: Reboot instruction received!" << endl ;
+}
+#endif //ENABLE_REBOOT
 
 
