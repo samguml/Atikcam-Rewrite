c27eba7628c553dbca548e7b2d51d731886dbe43
samguml <70164625+samguml@users.noreply.github.com>
2020-10-16 19:50:48 -0400

    Update camera_extern.h

--- include/camera_extern.h
+++ include/camera_extern.h
@@ -4,15 +4,16 @@
 @date 9/28/20
 @brief: contains extern defnitons for shared variables and synchronizers between modules
 */ 
-
-#include <camera.h>  //This may not be necessary, but is included just in case. May be subject to change 
-#include <pthread.h>
 #ifndef CAMERA_EXTERN_H
 #define CAMERA_EXTERN_H
-#endif
+#include <camera.h>  //This may not be necessary, but is included just in case. May be subject to change 
+#include <pthread.h>
+
 
 extern double  minshortexposure;
 
 extern double maxshortexposure;
 
 extern unsigned  pix_bin;
+
+#endif //CAMERA_EXTERN_H
