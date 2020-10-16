98dd7e7f15ade75e099b4589dde4de05c2dca1c5
samguml <70164625+samguml@users.noreply.github.com>
2020-10-16 19:51:26 -0400

    Update housekeeping_extern.h

--- include/housekeeping_extern.h
+++ include/housekeeping_extern.h
@@ -4,12 +4,11 @@
 @Date: 9/28/20
 @brief: contains extern definitons for shared variables and synchronizers between modules
 */ 
-
-#include <housekeeping.h> //This may not be necessary, but is included just in case. May be subject to change 
-#include <pthread.h>
 #ifndef HOUSEKEEPING_EXTERN_H
 #define HOUSEKEEPING_EXTERN_H
-#endif
+#include <housekeeping.h> //This may not be necessary, but is included just in case. May be subject to change 
+#include <pthread.h>
+
 
 //extern defintions for shared variables 
 
@@ -23,3 +22,4 @@ extern volatile unsigned long long camofftime:
 
 extern volatile bool ccdoverheat;
 
+#endif //HOUSEKEEPING_EXTERN_H
