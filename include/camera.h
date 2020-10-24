92845a192134b3b8a2a6bbf110cccb416a446e74
samguml <70164625+samguml@users.noreply.github.com>
2020-10-24 16:45:22 -0400

    Update camera.h

--- include/camera.h
+++ include/camera.h
@@ -62,25 +62,23 @@ typedef struct image {
 
 void convert_to_packet(image * a , datavis_p * b);
 
-unsigned long long int timenow();
-
 inline void put_data ( ostream & str , unsigned short val );
 inline void put_data ( ostream & str , unsigned long long int val );
 inline void put_data ( ostream & str , float val );
 inline void put_data ( ostream & str , char val ) ;
-  
+
 int save(const char *fileName, image* data);
- 
-void sys_reboot(void);
 
 char space_left(void);
  
+unsigned long long int timenow();
+
 double find_optimum_exposure ( unsigned short * picdata , unsigned int imgsize , double exposure );
-  
+
 bool snap_picture ( AtikCamera * device , unsigned pixX , unsigned pixY , unsigned short * data , double exposure  );
-  
-char space_left(void);
- 
+
+void sys_reboot(void);
+
 void * camera_thread(void *t);
   
   
