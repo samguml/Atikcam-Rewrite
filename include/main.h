39d72199e6bc732df1acd4a0bf0211b6d1b46e70
samguml <70164625+samguml@users.noreply.github.com>
2020-10-23 20:25:34 -0400

    Update main.h

--- include/main.h
+++ include/main.h
@@ -4,14 +4,10 @@
 @brief Holds extern definitions of done & thread local errno. Would contain error printer prototype, but that feature has been phased out.
 @Date 9/28/2020
 **/
-//Note all modules include main.h in their .c file
+
 
 #ifndef MAIN_H
 #define MAIN_H
-
-
-#include <signal.h>
-#include <fstream.h>//
 
 //Extern defintions for logs 
 extern ofstream templog ;
@@ -24,14 +20,8 @@ const int num_wakeups = sizeof(wakeups) / sizeof(pthread_cond_t *);
 //extern definition of done (controls loops for all threads)
 extern volatile sig_atomic_t done;
 
-//extern definition of thread local errno/custom 
-extern ofstream templog ;
-extern ofstream camlog ;
-extern ofstream errlog ;
-
 //extern defintion of local signal handler? 
 extern overheat(int signum);
-
 
 // interrupt handler for SIGINT
 void catch_sigint(int);
