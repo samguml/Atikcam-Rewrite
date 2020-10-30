5ef574fdf02d7c3ff8901b62001ed85f8922e023
samguml <samuel_gregory@student.uml.edu>
2020-10-29 20:23:24 -0400

    Makefile

---
+++ makefile.mk
@@ -0,0 +1,25 @@
+IDIR =../include
+CC=gcc
+CFLAGS =I$(IDIR)
+
+ODIR = obj 
+LDIR = ../include
+
+LIBS = -lm
+
+_DEPS = camera.h camera_extern.h camera_iface.h datavis.h datavis_extern.h datavis_iface.h housekeeping.h housekeeping_extern.h housekeeping_iface.h macros.h main.h modules.h
+DEPS = $(patsubst %, $(IDIR)/%,$(_DEPS))
+
+_OBJ =main.o datavis.o camera.o housekeeping.o
+OBJ = $(patsubst %, $(ODIR)/%,$(_OBJ))
+
+$(ODIR)/%.o: %.c $(DEPS)
+  $(CC) -c -o $@ $< $(CFLAGS)
+
+atikcameramake: $(OBJ)
+  $(CC) -o $@ $^ $(CFLAGS) $(LIBS)
+
+.PHONY: clean
+
+clean: 
+  rm -f $(ODIR)/*.o *~ core $(INCDIR)/*~
